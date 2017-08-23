f37aaac09713d42b08c095cf1f7e98fbeb9d73e6
Dane Stuckel <dane.stuckel@gmail.com>
2017-08-23 12:54:48 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const findDuplicateDependencies = require('find-duplicate-dependencies');
+
+findDuplicateDependencies().then(function (result) {
+  const keys = Object.keys(result);
+  for(let i = 0; i < keys.length; i++) {
+    const packageName = keys[i];
+    const list = result[packageName];
+
+    let str = packageName + ':\n';
+    str += list.forEach(item => '  ' + [item.version, item.path.split('/').join('->'), item.from].join(', ')).join('\n');
+
+    console.log(str);
+  }
+}).catch(function (err) {
+  console.error(err.stack);
+  return process.exit(1);
+});
